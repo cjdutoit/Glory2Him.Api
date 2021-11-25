96f671130971c6032e9d75d0a2ca292033a4a33c
Christo du Toit <christo@dutoit.co.uk>
2021-11-25 22:50:11 +0000

    DATA: Posts Closes #10

---
+++ G2H.Api.Web/Brokers/StorageBroker.cs
@@ -0,0 +1,45 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (c) Christo du Toit. All rights reserved.
+// Licensed under the MIT License.
+// See License.txt in the project root for license information.
+// FREE TO USE TO HELP SHARE THE GOSPEL
+// Mark 16:15 NIV "Go into all the world and preach the gospel to all creation."
+// https://mark.bible/mark-16-15 
+// --------------------------------------------------------------------------------
+
+
+using EFxceptions;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace G2H.Api.Web.Brokers.Storages
+{
+    public partial class StorageBroker : EFxceptionsContext, IStorageBroker
+    {
+        private readonly IConfiguration configuration;
+
+        public StorageBroker(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+            this.Database.Migrate();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            string connectionString = this.configuration
+                .GetConnectionString("DefaultConnection");
+
+            optionsBuilder.UseSqlServer(
+                connectionString,
+                sqlServerDbContextOptionsBuilder =>
+                    sqlServerDbContextOptionsBuilder.MigrationsAssembly("G2H.Api.Web"));
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+
+        public override void Dispose() { }
+    }
+}
---
+++ G2H.Api.Web/Models/Status/Status.cs
@@ -0,0 +1,31 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (c) Christo du Toit. All rights reserved.
+// Licensed under the MIT License.
+// See License.txt in the project root for license information.
+// FREE TO USE TO HELP SHARE THE GOSPEL
+// Mark 16:15 NIV "Go into all the world and preach the gospel to all creation."
+// https://mark.bible/mark-16-15 
+// --------------------------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Text.Json.Serialization;
+using G2H.Api.Web.Models.Base;
+using G2H.Api.Web.Models.Posts;
+
+namespace G2H.Api.Web.Models.Statuses
+{
+    public class Status : IAudit
+    {
+        public StatusId Id { get; set; }
+        public string Name { get; set; }
+
+        public Guid CreatedByUserId { get; set; }
+        public DateTimeOffset CreatedDate { get; set; }
+        public Guid UpdatedByUserId { get; set; }
+        public DateTimeOffset UpdatedDate { get; set; }
+
+        [JsonIgnore]
+        public virtual List<Post> Posts { get; set; } = new List<Post>();
+    }
+}
