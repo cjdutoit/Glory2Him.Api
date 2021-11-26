e9335a30e836f275fb29de60168c25e2f81c5a1c
Christo du Toit <christo@dutoit.co.uk>
2021-11-26 00:02:23 +0000

    DATA: PostComments

---
+++ G2H.Api.Web/Models/PostComments/PostComment.cs
@@ -0,0 +1,24 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (c) Christo du Toit. All rights reserved.
+// Licensed under the MIT License.
+// See License.txt in the project root for license information.
+// FREE TO USE TO HELP SHARE THE GOSPEL
+// Mark 16:15 NIV "Go into all the world and preach the gospel to all creation."
+// https://mark.bible/mark-16-15 
+// --------------------------------------------------------------------------------
+
+using System;
+using G2H.Api.Web.Models.Comments;
+using G2H.Api.Web.Models.Posts;
+
+namespace G2H.Api.Web.Models.PostsComments
+{
+    public class PostComment
+    {
+        public Guid PostId { get; set; }
+        public virtual Post Post { get; set; }
+
+        public Guid CommentId { get; set; }
+        public virtual Comment Comment { get; set; }
+    }
+}
