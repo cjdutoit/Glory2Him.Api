16f0d4ca95eb8a9a61ad08b9aa01cd39b19e233d
Christo du Toit <christo@dutoit.co.uk>
2021-12-04 23:21:43 +0000

    FOUNDATIONS: Implement Provisioning Resource Group

---
+++ G2H.Api.Infrastructure.Provision/Services/Foundations/CloudManagementService.cs
@@ -0,0 +1,44 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (c) Christo du Toit. All rights reserved.
+// Licensed under the MIT License.
+// See License.txt in the project root for license information.
+// FREE TO USE TO HELP SHARE THE GOSPEL
+// Mark 16:15 NIV "Go into all the world and preach the gospel to all creation."
+// https://mark.bible/mark-16-15 
+// --------------------------------------------------------------------------------
+
+using System.Threading.Tasks;
+using G2H.Api.Infrastructure.Provision.Brokers.Clouds;
+using G2H.Api.Infrastructure.Provision.Brokers.Loggings;
+using Microsoft.Azure.Management.ResourceManager.Fluent;
+
+namespace G2H.Api.Infrastructure.Provision.Services.Foundations
+{
+    public class CloudManagementService : ICloudManagementService
+    {
+        private readonly ICloudBroker cloudBroker;
+        private readonly ILoggingBroker loggingBroker;
+
+        public CloudManagementService()
+        {
+            this.cloudBroker = new CloudBroker();
+            this.loggingBroker = new LoggingBroker();
+        }
+
+        public async ValueTask<IResourceGroup> ProvisionResourceGroupAsync(
+            string projectName,
+            string environment)
+        {
+            string resourceGroupName = $"{projectName}-RESOURCES-{environment}".ToUpper();
+            this.loggingBroker.LogActivity(message: $"Provisioning {resourceGroupName}...");
+
+            IResourceGroup resourceGroup =
+                await this.cloudBroker.CreateResourceGroupAsync(
+                    resourceGroupName);
+
+            this.loggingBroker.LogActivity(message: $"{resourceGroupName} Provisioned.");
+
+            return resourceGroup;
+        }
+    }
+}
