b48702b783fff1a227a8202a254d64a9e5ecfdfc
Christo du Toit <christo@dutoit.co.uk>
2021-12-04 22:52:08 +0000

    BROKERS: Create App Service Plan

---
+++ G2H.Api.Infrastructure.Provision/Brokers/Clouds/ICloudBroker.Plans.cs
@@ -0,0 +1,22 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (c) Christo du Toit. All rights reserved.
+// Licensed under the MIT License.
+// See License.txt in the project root for license information.
+// FREE TO USE TO HELP SHARE THE GOSPEL
+// Mark 16:15 NIV "Go into all the world and preach the gospel to all creation."
+// https://mark.bible/mark-16-15 
+// --------------------------------------------------------------------------------
+
+using System.Threading.Tasks;
+using Microsoft.Azure.Management.AppService.Fluent;
+using Microsoft.Azure.Management.ResourceManager.Fluent;
+
+namespace G2H.Api.Infrastructure.Provision.Brokers.Clouds
+{
+    public partial interface ICloudBroker
+    {
+        ValueTask<IAppServicePlan> CreatePlanAsync(
+            string planName,
+            IResourceGroup resourceGroup);
+    }
+}
