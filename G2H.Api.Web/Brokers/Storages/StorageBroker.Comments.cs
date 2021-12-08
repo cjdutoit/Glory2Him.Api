a6827ae3c9d1ef4ab35dcbacbfdfe288ef96233f
Christo du Toit <christo@dutoit.co.uk>
2021-12-08 19:45:58 +0000

    BROKERS: Insert Comment

---
+++ G2H.Api.Web/Brokers/Storages/StorageBroker.Comments.cs
@@ -0,0 +1,34 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (c) Christo du Toit. All rights reserved.
+// Licensed under the MIT License.
+// See License.txt in the project root for license information.
+// FREE TO USE TO HELP SHARE THE GOSPEL
+// Mark 16:15 NIV "Go into all the world and preach the gospel to all creation."
+// https://mark.bible/mark-16-15 
+// --------------------------------------------------------------------------------
+
+using System.Threading.Tasks;
+using G2H.Api.Web.Models.Comments;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+
+namespace G2H.Api.Web.Brokers.Storages
+{
+    public partial class StorageBroker
+    {
+        public DbSet<Comment> Comments { get; set; }
+
+        public async ValueTask<Comment> InsertCommentAsync(Comment comment)
+        {
+            using var broker =
+                new StorageBroker(this.configuration);
+
+            EntityEntry<Comment> commentEntityEntry =
+                await broker.Comments.AddAsync(comment);
+
+            await broker.SaveChangesAsync();
+
+            return commentEntityEntry.Entity;
+        }
+    }
+}
