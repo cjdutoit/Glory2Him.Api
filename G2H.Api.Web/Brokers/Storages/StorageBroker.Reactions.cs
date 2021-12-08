e843d9e366442e563fb564892052db055332f176
Christo du Toit <christo@dutoit.co.uk>
2021-12-08 17:38:13 +0000

    BROKERS: Select All Reactions

--- G2H.Api.Web/Brokers/Storages/StorageBroker.Reactions.cs
+++ G2H.Api.Web/Brokers/Storages/StorageBroker.Reactions.cs
@@ -7,6 +7,7 @@
 // https://mark.bible/mark-16-15 
 // --------------------------------------------------------------------------------
 
+using System.Linq;
 using System.Threading.Tasks;
 using G2H.Api.Web.Models.Reactions;
 using Microsoft.EntityFrameworkCore;
@@ -29,6 +30,14 @@ namespace G2H.Api.Web.Brokers.Storages
             await broker.SaveChangesAsync();
 
             return reactionEntityEntry.Entity;
+        }
+
+        public IQueryable<Reaction> SelectAllReactions()
+        {
+            using var broker =
+                new StorageBroker(configuration);
+
+            return broker.Reactions;
         }
     }
 }
