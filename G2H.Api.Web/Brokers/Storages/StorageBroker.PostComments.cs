203406705b9020d945b8b2ac58a3e8c9300e9b51
Christo du Toit <christo@dutoit.co.uk>
2021-12-09 00:08:26 +0000

    BROKERS: Select All Post Comments

--- G2H.Api.Web/Brokers/Storages/StorageBroker.PostComments.cs
+++ G2H.Api.Web/Brokers/Storages/StorageBroker.PostComments.cs
@@ -7,6 +7,7 @@
 // https://mark.bible/mark-16-15 
 // --------------------------------------------------------------------------------
 
+using System.Linq;
 using System.Threading.Tasks;
 using G2H.Api.Web.Models.PostsComments;
 using Microsoft.EntityFrameworkCore;
@@ -29,6 +30,14 @@ namespace G2H.Api.Web.Brokers.Storages
             await broker.SaveChangesAsync();
 
             return postCommentEntityEntry.Entity;
+        }
+
+        public IQueryable<PostComment> SelectAllPostComments()
+        {
+            using var broker =
+                new StorageBroker(this.configuration);
+
+            return broker.PostComments;
         }
     }
 }
