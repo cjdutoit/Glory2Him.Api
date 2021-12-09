7e8bc482437f5cc9a207f7e17e3b30f60afbb76e
Christo du Toit <christo@dutoit.co.uk>
2021-12-09 17:24:48 +0000

    BROKERS: Insert Post Reaction

---
+++ G2H.Api.Web/Brokers/Storages/StorageBroker.PostReactions.cs
@@ -0,0 +1,34 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (c) Christo du Toit. All rights reserved.
+// Licensed under the MIT License.
+// See License.txt in the project root for license information.
+// FREE TO USE TO HELP SHARE THE GOSPEL
+// Mark 16:15 NIV "Go into all the world and preach the gospel to all creation."
+// https://mark.bible/mark-16-15 
+// --------------------------------------------------------------------------------
+
+using System.Threading.Tasks;
+using G2H.Api.Web.Models.PostReactions;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+
+namespace G2H.Api.Web.Brokers.Storages
+{
+    public partial class StorageBroker
+    {
+        public DbSet<PostReaction> PostReactions { get; set; }
+
+        public async ValueTask<PostReaction> InsertPostReactionAsync(PostReaction postReaction)
+        {
+            using var broker =
+                new StorageBroker(this.configuration);
+
+            EntityEntry<PostReaction> postReactionEntityEntry =
+                await broker.PostReactions.AddAsync(postReaction);
+
+            await broker.SaveChangesAsync();
+
+            return postReactionEntityEntry.Entity;
+        }
+    }
+}
