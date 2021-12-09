fa774b6d3c1938e881fde374796e2234e2b31937
Christo du Toit <christo@dutoit.co.uk>
2021-12-09 18:44:02 +0000

    BROKERS: Select Approval By Id

--- G2H.Api.Web/Brokers/Storages/StorageBroker.Approvals.cs
+++ G2H.Api.Web/Brokers/Storages/StorageBroker.Approvals.cs
@@ -7,6 +7,7 @@
 // https://mark.bible/mark-16-15 
 // --------------------------------------------------------------------------------
 
+using System;
 using System.Linq;
 using System.Threading.Tasks;
 using G2H.Api.Web.Models.Approvals;
@@ -38,6 +39,14 @@ namespace G2H.Api.Web.Brokers.Storages
                 new StorageBroker(this.configuration);
 
             return broker.Approvals;
+        }
+
+        public async ValueTask<Approval> SelectApprovalByIdAsync(Guid approvalId)
+        {
+            using var broker =
+                new StorageBroker(this.configuration);
+
+            return await broker.Approvals.FindAsync(approvalId);
         }
     }
 }
