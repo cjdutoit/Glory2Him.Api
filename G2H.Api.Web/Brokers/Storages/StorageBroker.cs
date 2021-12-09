c6fd3ff330b4d95194a34a189db6ccf008fb0324
Christo du Toit <christo@dutoit.co.uk>
2021-12-09 18:55:15 +0000

    BROKERS: Insert Approval User

--- G2H.Api.Web/Brokers/Storages/StorageBroker.cs
+++ G2H.Api.Web/Brokers/Storages/StorageBroker.cs
@@ -28,6 +28,7 @@ namespace G2H.Api.Web.Brokers.Storages
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             AddApprovalReferences(modelBuilder);
+            AddApprovalUserReferences(modelBuilder);
             AddAttachmentReferences(modelBuilder);
             AddCommentCommentReferences(modelBuilder);
             AddCommentReactionReferences(modelBuilder);
