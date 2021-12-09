8ff7b85c434ae291fc46a2363796a5f604bec398
Christo du Toit <christo@dutoit.co.uk>
2021-12-09 17:46:35 +0000

    BROKERS: Insert Comment Reaction

---
+++ G2H.Api.Web/Brokers/Storages/StorageBroker.CommentReactions.cs
@@ -0,0 +1,34 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (c) Christo du Toit. All rights reserved.
+// Licensed under the MIT License.
+// See License.txt in the project root for license information.
+// FREE TO USE TO HELP SHARE THE GOSPEL
+// Mark 16:15 NIV "Go into all the world and preach the gospel to all creation."
+// https://mark.bible/mark-16-15 
+// --------------------------------------------------------------------------------
+
+using System.Threading.Tasks;
+using G2H.Api.Web.Models.PostReactions;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+
+namespace G2H.Api.Web.Brokers.Storages
+{
+    public partial class StorageBroker
+    {
+        public DbSet<CommentReaction> CommentReactions { get; set; }
+
+        public async ValueTask<CommentReaction> InsertCommentReactionAsync(CommentReaction commentReaction)
+        {
+            using var broker =
+                new StorageBroker(this.configuration);
+
+            EntityEntry<CommentReaction> commentReactionEntityEntry =
+                await broker.CommentReactions.AddAsync(commentReaction);
+
+            await broker.SaveChangesAsync();
+
+            return commentReactionEntityEntry.Entity;
+        }
+    }
+}
