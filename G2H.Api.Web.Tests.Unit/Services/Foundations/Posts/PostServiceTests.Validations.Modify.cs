b1fa1995db770573c686e8ccc1ba6e31866238e8
Christo du Toit <christo@dutoit.co.uk>
2021-12-28 19:15:12 +0000

    ShouldThrowValidationExceptionOnModifyIfPostIsNullAndLogItAsync -> FAIL

---
+++ G2H.Api.Web.Tests.Unit/Services/Foundations/Posts/PostServiceTests.Validations.Modify.cs
@@ -0,0 +1,52 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (c) Christo du Toit. All rights reserved.
+// Licensed under the MIT License.
+// See License.txt in the project root for license information.
+// FREE TO USE TO HELP SHARE THE GOSPEL
+// Mark 16:15 NIV "Go into all the world and preach the gospel to all creation."
+// https://mark.bible/mark-16-15 
+// --------------------------------------------------------------------------------
+
+using System.Threading.Tasks;
+using G2H.Api.Web.Models.Posts;
+using G2H.Api.Web.Models.Posts.Exceptions;
+using Moq;
+using Xunit;
+
+namespace G2H.Api.Web.Tests.Unit.Services.Foundations.Posts
+{
+    public partial class PostServiceTests
+    {
+        [Fact]
+        public async Task ShouldThrowValidationExceptionOnModifyIfPostIsNullAndLogItAsync()
+        {
+            // given
+            Post nullPost = null;
+            var nullPostException = new NullPostException();
+
+            var expectedPostValidationException =
+                new PostValidationException(nullPostException);
+
+            // when
+            ValueTask<Post> modifyPostTask =
+                this.postService.ModifyPostAsync(nullPost);
+
+            // then
+            await Assert.ThrowsAsync<PostValidationException>(() =>
+                modifyPostTask.AsTask());
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(
+                    expectedPostValidationException))),
+                        Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.UpdatePostAsync(It.IsAny<Post>()),
+                    Times.Never);
+
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
