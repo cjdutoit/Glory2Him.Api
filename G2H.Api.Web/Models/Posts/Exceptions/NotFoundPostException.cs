aef20e4e99848626d5f82dc27ac05edabdb40cf4
Christo du Toit <christo@dutoit.co.uk>
2021-12-28 18:16:31 +0000

    ShouldThrowNotFoundExceptionOnRetrieveByIdIfPostIsNotFoundAndLogItAsync -> FAIL

---
+++ G2H.Api.Web/Models/Posts/Exceptions/NotFoundPostException.cs
@@ -0,0 +1,21 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (c) Christo du Toit. All rights reserved.
+// Licensed under the MIT License.
+// See License.txt in the project root for license information.
+// FREE TO USE TO HELP SHARE THE GOSPEL
+// Mark 16:15 NIV "Go into all the world and preach the gospel to all creation."
+// https://mark.bible/mark-16-15 
+// --------------------------------------------------------------------------------
+
+using System;
+using Xeptions;
+
+namespace G2H.Api.Web.Models.Posts.Exceptions
+{
+    public class NotFoundPostException : Xeption
+    {
+        public NotFoundPostException(Guid postId)
+            : base(message: $"Couldn't find post with postId: {postId}.")
+        { }
+    }
+}
