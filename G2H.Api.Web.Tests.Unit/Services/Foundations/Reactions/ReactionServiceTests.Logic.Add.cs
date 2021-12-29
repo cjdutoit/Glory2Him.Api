dc0cd95e202e335f1d248285e16d8c6237708433
Christo du Toit <christo@dutoit.co.uk>
2021-12-29 23:34:44 +0000

    ShouldAddReactionAsync -> FAIL

---
+++ G2H.Api.Web.Tests.Unit/Services/Foundations/Reactions/ReactionServiceTests.Logic.Add.cs
@@ -0,0 +1,62 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (c) Christo du Toit. All rights reserved.
+// Licensed under the MIT License.
+// See License.txt in the project root for license information.
+// FREE TO USE TO HELP SHARE THE GOSPEL
+// Mark 16:15 NIV "Go into all the world and preach the gospel to all creation."
+// https://mark.bible/mark-16-15 
+// --------------------------------------------------------------------------------
+
+using System;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Force.DeepCloner;
+using G2H.Api.Web.Models.Reactions;
+using Moq;
+using Xunit;
+
+namespace G2H.Api.Web.Tests.Unit.Services.Foundations.Reactions
+{
+    public partial class ReactionServiceTests
+    {
+        [Fact]
+        public async Task ShouldAddReactionAsync()
+        {
+            // given
+            DateTimeOffset randomDateTimeOffset =
+                GetRandomDateTimeOffset();
+
+            Reaction randomReaction = CreateRandomReaction(randomDateTimeOffset);
+            Reaction inputReaction = randomReaction;
+            Reaction storageReaction = inputReaction;
+            Reaction expectedReaction = storageReaction.DeepClone();
+
+            this.dateTimeBrokerMock.Setup(broker =>
+                broker.GetCurrentDateTimeOffset())
+                    .Returns(randomDateTimeOffset);
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.InsertReactionAsync(inputReaction))
+                    .ReturnsAsync(storageReaction);
+
+            // when
+            Reaction actualReaction = await this.reactionService
+                .AddReactionAsync(inputReaction);
+
+            // then
+            actualReaction.Should().BeEquivalentTo(expectedReaction);
+
+            this.dateTimeBrokerMock.Verify(broker =>
+                broker.GetCurrentDateTimeOffset(),
+                    Times.Once());
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.InsertReactionAsync(inputReaction),
+                    Times.Once);
+
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
---
+++ G2H.Api.Web.Tests.Unit/Services/Foundations/Reactions/ReactionServiceTests.cs
@@ -0,0 +1,56 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (c) Christo du Toit. All rights reserved.
+// Licensed under the MIT License.
+// See License.txt in the project root for license information.
+// FREE TO USE TO HELP SHARE THE GOSPEL
+// Mark 16:15 NIV "Go into all the world and preach the gospel to all creation."
+// https://mark.bible/mark-16-15 
+// --------------------------------------------------------------------------------
+
+using System;
+using G2H.Api.Web.Brokers.DateTimes;
+using G2H.Api.Web.Brokers.Loggings;
+using G2H.Api.Web.Brokers.Storages;
+using G2H.Api.Web.Models.Reactions;
+using G2H.Api.Web.Services.Foundations.Reactions;
+using Moq;
+using Tynamix.ObjectFiller;
+
+namespace G2H.Api.Web.Tests.Unit.Services.Foundations.Reactions
+{
+    public partial class ReactionServiceTests
+    {
+        private readonly Mock<IStorageBroker> storageBrokerMock;
+        private readonly Mock<IDateTimeBroker> dateTimeBrokerMock;
+        private readonly Mock<ILoggingBroker> loggingBrokerMock;
+        private readonly IReactionService reactionService;
+
+        public ReactionServiceTests()
+        {
+            this.storageBrokerMock = new Mock<IStorageBroker>();
+            this.dateTimeBrokerMock = new Mock<IDateTimeBroker>();
+            this.loggingBrokerMock = new Mock<ILoggingBroker>();
+
+            this.reactionService = new ReactionService(
+                storageBroker: this.storageBrokerMock.Object,
+                dateTimeBroker: this.dateTimeBrokerMock.Object,
+                loggingBroker: this.loggingBrokerMock.Object);
+        }
+
+        private static DateTimeOffset GetRandomDateTimeOffset() =>
+            new DateTimeRange(earliestDate: new DateTime()).GetValue();
+
+        private static Reaction CreateRandomReaction(DateTimeOffset dateTimeOffset) =>
+            CreateReactionFiller(dateTimeOffset).Create();
+
+        private static Filler<Reaction> CreateReactionFiller(DateTimeOffset dateTimeOffset)
+        {
+            var filler = new Filler<Reaction>();
+
+            filler.Setup()
+                .OnType<DateTimeOffset>().Use(dateTimeOffset);
+
+            return filler;
+        }
+    }
+}
