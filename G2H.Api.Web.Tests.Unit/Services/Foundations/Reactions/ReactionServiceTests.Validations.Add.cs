4f09f627c286f91a5435747d255a19f33c57b00a
Christo du Toit <christo@dutoit.co.uk>
2021-12-30 01:08:34 +0000

    ShouldThrowValidationExceptionOnAddIfReactionIsNullAndLogItAsync -> FAIL

---
+++ G2H.Api.Web.Tests.Unit/Services/Foundations/Reactions/ReactionServiceTests.Validations.Add.cs
@@ -0,0 +1,50 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (c) Christo du Toit. All rights reserved.
+// Licensed under the MIT License.
+// See License.txt in the project root for license information.
+// FREE TO USE TO HELP SHARE THE GOSPEL
+// Mark 16:15 NIV "Go into all the world and preach the gospel to all creation."
+// https://mark.bible/mark-16-15 
+// --------------------------------------------------------------------------------
+
+using System.Threading.Tasks;
+using G2H.Api.Web.Models.Reactions;
+using G2H.Api.Web.Models.Reactions.Exceptions;
+using Moq;
+using Xunit;
+
+namespace G2H.Api.Web.Tests.Unit.Services.Foundations.Reactions
+{
+    public partial class ReactionServiceTests
+    {
+        [Fact]
+        public async Task ShouldThrowValidationExceptionOnAddIfReactionIsNullAndLogItAsync()
+        {
+            // given
+            Reaction nullReaction = null;
+
+            var nullReactionException =
+                new NullReactionException();
+
+            var expectedReactionValidationException =
+                new ReactionValidationException(nullReactionException);
+
+            // when
+            ValueTask<Reaction> addReactionTask =
+                this.reactionService.AddReactionAsync(nullReaction);
+
+            // then
+            await Assert.ThrowsAsync<ReactionValidationException>(() =>
+                addReactionTask.AsTask());
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(
+                    expectedReactionValidationException))),
+                        Times.Once);
+
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
