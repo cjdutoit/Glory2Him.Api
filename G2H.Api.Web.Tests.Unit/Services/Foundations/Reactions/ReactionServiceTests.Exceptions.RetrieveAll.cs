67a0543bcacd42b2660bebe2786b52d15af362fc
Christo du Toit <christo@dutoit.co.uk>
2021-12-30 13:35:00 +0000

    ShouldThrowCriticalDependencyExceptionOnRetrieveAllWhenSqlExceptionOccursAndLogIt -> FAIL

---
+++ G2H.Api.Web.Tests.Unit/Services/Foundations/Reactions/ReactionServiceTests.Exceptions.RetrieveAll.cs
@@ -0,0 +1,58 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (c) Christo du Toit. All rights reserved.
+// Licensed under the MIT License.
+// See License.txt in the project root for license information.
+// FREE TO USE TO HELP SHARE THE GOSPEL
+// Mark 16:15 NIV "Go into all the world and preach the gospel to all creation."
+// https://mark.bible/mark-16-15 
+// --------------------------------------------------------------------------------
+
+using System;
+using G2H.Api.Web.Models.Reactions.Exceptions;
+using Microsoft.Data.SqlClient;
+using Moq;
+using Xunit;
+
+namespace G2H.Api.Web.Tests.Unit.Services.Foundations.Reactions
+{
+    public partial class ReactionServiceTests
+    {
+        [Fact]
+        public void ShouldThrowCriticalDependencyExceptionOnRetrieveAllWhenSqlExceptionOccursAndLogIt()
+        {
+            // given
+            SqlException sqlException = GetSqlException();
+
+            var failedStorageException =
+                new FailedReactionStorageException(sqlException);
+
+            var expectedReactionDependencyException =
+                new ReactionDependencyException(failedStorageException);
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.SelectAllReactions())
+                    .Throws(sqlException);
+
+            // when
+            Action retrieveAllReactionsAction = () =>
+                this.reactionService.RetrieveAllReactions();
+
+            // then
+            Assert.Throws<ReactionDependencyException>(
+                retrieveAllReactionsAction);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectAllReactions(),
+                    Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogCritical(It.Is(SameExceptionAs(
+                    expectedReactionDependencyException))),
+                        Times.Once);
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
