509f327e36b90ed143eb550ed4b98cc8b3021618
Christo du Toit <christo@dutoit.co.uk>
2021-12-30 20:08:51 +0000

    ShouldThrowCriticalDependencyExceptionOnModifyIfSqlErrorOccursAndLogItAsync -> PASS

---
+++ G2H.Api.Web.Tests.Unit/Services/Foundations/Reactions/ReactionServiceTests.Exceptions.Modify.cs
@@ -0,0 +1,68 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (c) Christo du Toit. All rights reserved.
+// Licensed under the MIT License.
+// See License.txt in the project root for license information.
+// FREE TO USE TO HELP SHARE THE GOSPEL
+// Mark 16:15 NIV "Go into all the world and preach the gospel to all creation."
+// https://mark.bible/mark-16-15 
+// --------------------------------------------------------------------------------
+
+using System.Threading.Tasks;
+using G2H.Api.Web.Models.Reactions;
+using G2H.Api.Web.Models.Reactions.Exceptions;
+using Microsoft.Data.SqlClient;
+using Moq;
+using Xunit;
+
+namespace G2H.Api.Web.Tests.Unit.Services.Foundations.Reactions
+{
+    public partial class ReactionServiceTests
+    {
+        [Fact]
+        public async Task ShouldThrowCriticalDependencyExceptionOnModifyIfSqlErrorOccursAndLogItAsync()
+        {
+            // given
+            Reaction randomReaction = CreateRandomReaction();
+            SqlException sqlException = GetSqlException();
+
+            var failedReactionStorageException =
+                new FailedReactionStorageException(sqlException);
+
+            var expectedReactionDependencyException =
+                new ReactionDependencyException(failedReactionStorageException);
+
+            this.dateTimeBrokerMock.Setup(broker =>
+                broker.GetCurrentDateTimeOffset())
+                    .Throws(sqlException);
+
+            // when
+            ValueTask<Reaction> addReactionTask =
+                this.reactionService.ModifyReactionAsync(randomReaction);
+
+            // then
+            await Assert.ThrowsAsync<ReactionDependencyException>(() =>
+               addReactionTask.AsTask());
+
+            this.dateTimeBrokerMock.Verify(broker =>
+                broker.GetCurrentDateTimeOffset(),
+                    Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectReactionByIdAsync(randomReaction.Id),
+                    Times.Never);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogCritical(It.Is(SameExceptionAs(
+                    expectedReactionDependencyException))),
+                        Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.UpdateReactionAsync(randomReaction),
+                    Times.Never);
+
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
