00358f8f95f4df1f6bc11bf101eed393d0206b7b
Christo du Toit <christo@dutoit.co.uk>
2022-01-03 14:35:45 +0000

    BROKERS: Select Status By Id Closes #230

--- G2H.Api.Web/Brokers/Storages/IStorageBroker.Statuses.cs
+++ G2H.Api.Web/Brokers/Storages/IStorageBroker.Statuses.cs
@@ -7,6 +7,7 @@
 // https://mark.bible/mark-16-15 
 // --------------------------------------------------------------------------------
 
+using System;
 using System.Linq;
 using System.Threading.Tasks;
 using G2H.Api.Web.Models.Statuses;
@@ -17,5 +18,6 @@ namespace G2H.Api.Web.Brokers.Storages
     {
         ValueTask<Status> InsertStatusAsync(Status status);
         IQueryable<Status> SelectAllStatuses();
+        ValueTask<Status> SelectStatusByIdAsync(Guid statusId);
     }
 }
