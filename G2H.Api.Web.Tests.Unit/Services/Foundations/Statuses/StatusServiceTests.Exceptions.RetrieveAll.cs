fc2619c054335483525cf7ccb01bea5211d74976
Christo du Toit <Christo.DuToit@gstt.nhs.uk>
2022-01-05 10:48:21 +0000

    ShouldThrowCriticalDependencyExceptionOnRetrieveAllWhenSqlExceptionOccursAndLogIt -> FAIL

---
+++ G2H.Api.Web.Tests.Unit/Services/Foundations/Statuses/StatusServiceTests.Exceptions.RetrieveAll.cs
@@ -0,0 +1,58 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (c) Christo du Toit. All rights reserved.
+// Licensed under the MIT License.
+// See License.txt in the project root for license information.
+// FREE TO USE TO HELP SHARE THE GOSPEL
+// Mark 16:15 NIV "Go into all the world and preach the gospel to all creation."
+// https://mark.bible/mark-16-15 
+// --------------------------------------------------------------------------------
+
+using System;
+using G2H.Api.Web.Models.Statuses.Exceptions;
+using Microsoft.Data.SqlClient;
+using Moq;
+using Xunit;
+
+namespace G2H.Api.Web.Tests.Unit.Services.Foundations.Statuses
+{
+    public partial class StatusServiceTests
+    {
+        [Fact]
+        public void ShouldThrowCriticalDependencyExceptionOnRetrieveAllWhenSqlExceptionOccursAndLogIt()
+        {
+            // given
+            SqlException sqlException = GetSqlException();
+
+            var failedStorageException =
+                new FailedStatusStorageException(sqlException);
+
+            var expectedStatusDependencyException =
+                new StatusDependencyException(failedStorageException);
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.SelectAllStatuses())
+                    .Throws(sqlException);
+
+            // when
+            Action retrieveAllStatusesAction = () =>
+                this.statusService.RetrieveAllStatuses();
+
+            // then
+            Assert.Throws<StatusDependencyException>(
+                retrieveAllStatusesAction);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectAllStatuses(),
+                    Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogCritical(It.Is(SameExceptionAs(
+                    expectedStatusDependencyException))),
+                        Times.Once);
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
