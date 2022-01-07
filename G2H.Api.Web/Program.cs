8b554dfe4c983c6c4413f2b78319f5e27a5ad126
Christo du Toit <christo@dutoit.co.uk>
2022-01-07 22:56:31 +0000

    CODE RUB: Removed User Secrets CODE RUB: Cleanup Solution

--- G2H.Api.Web/Program.cs
+++ G2H.Api.Web/Program.cs
@@ -8,7 +8,6 @@
 // --------------------------------------------------------------------------------
 
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 
 namespace G2H.Api.Web
@@ -22,13 +21,6 @@ namespace G2H.Api.Web
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
-            .ConfigureAppConfiguration((hostingContext, config) =>
-                {
-                    if (hostingContext.HostingEnvironment.IsDevelopment())
-                    {
-                        config.AddUserSecrets<Program>();
-                    }
-                })
             .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
