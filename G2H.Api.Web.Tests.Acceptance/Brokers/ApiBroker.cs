cf274ed4aa4897a3c1328fd6e78c0e927813de19
Christo du Toit <christo@dutoit.co.uk>
2022-01-07 19:40:00 +0000

    WIP

---
+++ G2H.Api.Web.Tests.Acceptance/Brokers/ApiBroker.cs
@@ -0,0 +1,35 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (c) Christo du Toit. All rights reserved.
+// Licensed under the MIT License.
+// See License.txt in the project root for license information.
+// FREE TO USE TO HELP SHARE THE GOSPEL
+// Mark 16:15 NIV "Go into all the world and preach the gospel to all creation."
+// https://mark.bible/mark-16-15 
+// --------------------------------------------------------------------------------
+
+using System.Net.Http;
+using G2H.Api.Web.Services.Foundations.Statuses;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.DependencyInjection;
+using RESTFulSense.Clients;
+
+namespace G2H.Api.Web.Tests.Acceptance.Brokers
+{
+    public partial class ApiBroker
+    {
+        private readonly WebApplicationFactory<Startup> webApplicationFactory;
+        private readonly HttpClient httpClient;
+        private readonly IRESTFulApiFactoryClient apiFactoryClient;
+        internal readonly StatusService StatusService;
+
+        public ApiBroker()
+        {
+            this.webApplicationFactory = new WebApplicationFactory<Startup>();
+
+            var x = webApplicationFactory.Services.GetService<IStatusService>();
+
+            this.httpClient = this.webApplicationFactory.CreateClient();
+            this.apiFactoryClient = new RESTFulApiFactoryClient(this.httpClient);
+        }
+    }
+}
