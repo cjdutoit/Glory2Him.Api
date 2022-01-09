0d5a92b18d6035248ed5230251fee3c738c233c6
Christo du Toit <christo@dutoit.co.uk>
2022-01-09 20:52:18 +0000

    CONTROLLERS: GET All Post Types

---
+++ G2H.Api.Web/Controllers/PostTypesController.cs
@@ -0,0 +1,48 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (c) Christo du Toit. All rights reserved.
+// Licensed under the MIT License.
+// See License.txt in the project root for license information.
+// FREE TO USE TO HELP SHARE THE GOSPEL
+// Mark 16:15 NIV "Go into all the world and preach the gospel to all creation."
+// https://mark.bible/mark-16-15 
+// --------------------------------------------------------------------------------
+
+using System.Linq;
+using G2H.Api.Web.Models.PostTypes;
+using G2H.Api.Web.Models.PostTypes.Exceptions;
+using G2H.Api.Web.Services.Foundations.PostTypes;
+using Microsoft.AspNetCore.Mvc;
+using RESTFulSense.Controllers;
+
+namespace G2H.Api.Web.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class PostTypesController : RESTFulController
+    {
+        private readonly IPostTypeService postTypeService;
+
+        public PostTypesController(IPostTypeService postTypeService) =>
+            this.postTypeService = postTypeService;
+
+        [HttpGet]
+        public ActionResult<IQueryable<PostType>> GetAllPostTypes()
+        {
+            try
+            {
+                IQueryable<PostType> retrievedPostTypes =
+                    this.postTypeService.RetrieveAllPostTypes();
+
+                return Ok(retrievedPostTypes);
+            }
+            catch (PostTypeDependencyException postTypeDependencyException)
+            {
+                return InternalServerError(postTypeDependencyException);
+            }
+            catch (PostTypeServiceException postTypeServiceException)
+            {
+                return InternalServerError(postTypeServiceException);
+            }
+        }
+    }
+}
