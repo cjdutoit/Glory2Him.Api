712393bbb21208d33c95d1fc5e623e4482bfda72
Christo du Toit <christo@dutoit.co.uk>
2022-01-09 19:45:15 +0000

    CODE RUB: Removed duplicate code

--- G2H.Api.Web.Tests.Unit/Services/Foundations/Statuses/StatusServiceTests.Validations.Modify.cs
+++ G2H.Api.Web.Tests.Unit/Services/Foundations/Statuses/StatusServiceTests.Validations.Modify.cs
@@ -169,7 +169,7 @@ namespace G2H.Api.Web.Tests.Unit.Services.Foundations.Statuses
         }
 
         [Theory]
-        [MemberData(nameof(InvalidMinuteCases))]
+        [MemberData(nameof(MinutesBeforeOrAfter))]
         public async Task ShouldThrowValidationExceptionOnModifyIfUpdatedDateIsNotRecentAndLogItAsync(int minutes)
         {
             // given
