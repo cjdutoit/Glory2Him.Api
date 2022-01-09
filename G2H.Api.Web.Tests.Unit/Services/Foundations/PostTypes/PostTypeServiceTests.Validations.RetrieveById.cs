7429e1ae92ffcc015ff1be9d649d52dc92debc2e
Christo du Toit <christo@dutoit.co.uk>
2022-01-09 14:18:46 +0000

    ShouldThrowValidationExceptionOnRetrieveByIdIfIdIsInvalidAndLogItAsync -> FAIL

---
+++ G2H.Api.Web.Tests.Unit/Services/Foundations/PostTypes/PostTypeServiceTests.Validations.RetrieveById.cs
@@ -0,0 +1,58 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (c) Christo du Toit. All rights reserved.
+// Licensed under the MIT License.
+// See License.txt in the project root for license information.
+// FREE TO USE TO HELP SHARE THE GOSPEL
+// Mark 16:15 NIV "Go into all the world and preach the gospel to all creation."
+// https://mark.bible/mark-16-15 
+// --------------------------------------------------------------------------------
+
+using System.Threading.Tasks;
+using G2H.Api.Web.Models.PostTypes;
+using G2H.Api.Web.Models.PostTypes.Exceptions;
+using Moq;
+using Xunit;
+
+namespace G2H.Api.Web.Tests.Unit.Services.Foundations.PostTypes
+{
+    public partial class PostTypeServiceTests
+    {
+        [Fact]
+        public async Task ShouldThrowValidationExceptionOnRetrieveByIdIfIdIsInvalidAndLogItAsync()
+        {
+            // given
+            var invalidPostTypeId = (PostTypeId)default;
+
+            var invalidPostTypeException =
+                new InvalidPostTypeException();
+
+            invalidPostTypeException.AddData(
+                key: nameof(PostType.Id),
+                values: "Id is required");
+
+            var expectedPostTypeValidationException =
+                new PostTypeValidationException(invalidPostTypeException);
+
+            // when
+            ValueTask<PostType> retrievePostTypeByIdTask =
+                this.postTypeService.RetrievePostTypeByIdAsync(invalidPostTypeId);
+
+            // then
+            await Assert.ThrowsAsync<PostTypeValidationException>(() =>
+                retrievePostTypeByIdTask.AsTask());
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(
+                    expectedPostTypeValidationException))),
+                        Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectPostTypeByIdAsync(It.IsAny<PostTypeId>()),
+                    Times.Never);
+
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
