63c180456fd7c97ec75b6045556a218a6aebe866
Christo du Toit <christo@dutoit.co.uk>
2022-01-09 14:57:33 +0000

    ShouldThrowValidationExceptionOnModifyIfPostTypeIsNullAndLogItAsync -> FAIL

---
+++ G2H.Api.Web.Tests.Unit/Services/Foundations/PostTypes/PostTypeServiceTests.Validations.Modify.cs
@@ -0,0 +1,52 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (c) Christo du Toit. All rights reserved.
+// Licensed under the MIT License.
+// See License.txt in the project root for license information.
+// FREE TO USE TO HELP SHARE THE GOSPEL
+// Mark 16:15 NIV "Go into all the world and preach the gospel to all creation."
+// https://mark.bible/mark-16-15 
+// --------------------------------------------------------------------------------
+
+using System.Threading.Tasks;
+using G2H.Api.Web.Models.PostTypes;
+using G2H.Api.Web.Models.PostTypes.Exceptions;
+using Moq;
+using Xunit;
+
+namespace G2H.Api.Web.Tests.Unit.Services.Foundations.PostTypes
+{
+    public partial class PostTypeServiceTests
+    {
+        [Fact]
+        public async Task ShouldThrowValidationExceptionOnModifyIfPostTypeIsNullAndLogItAsync()
+        {
+            // given
+            PostType nullPostType = null;
+            var nullPostTypeException = new NullPostTypeException();
+
+            var expectedPostTypeValidationException =
+                new PostTypeValidationException(nullPostTypeException);
+
+            // when
+            ValueTask<PostType> modifyPostTypeTask =
+                this.postTypeService.ModifyPostTypeAsync(nullPostType);
+
+            // then
+            await Assert.ThrowsAsync<PostTypeValidationException>(() =>
+                modifyPostTypeTask.AsTask());
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(
+                    expectedPostTypeValidationException))),
+                        Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.UpdatePostTypeAsync(It.IsAny<PostType>()),
+                    Times.Never);
+
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
